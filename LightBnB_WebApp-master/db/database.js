d2079ed94ca9d036113394efb5af383f4f5cd077
Chaahat <arorac2@mymacewan.ca>
2023-06-01 13:10:45 -0600

    update remaining functions

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -11,14 +11,10 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.l
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => result.rows[0])
+  .catch((err) => console.log(err.message));
 };
 
 /**
@@ -27,8 +23,12 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => result.rows[0])
+  .catch((err) => console.log(err.message));
 };
+
 
 /**
  * Add a new user to the database.
@@ -36,10 +36,13 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
+      [user.name, user.email, user.password]
+    )
+    .then((result) => result.rows[0])
+    .catch((err) => console.log(err.message));
 };
 
 /// Reservations
@@ -50,8 +53,15 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(
+    `SELECT * FROM reservations WHERE guest_id = $1 LIMIT $2`,
+    [guest_id, limit]
+  )
+  .then((result) => result.rows)
+  .catch((err) => console.log(err.message));
 };
+
 
 /// Properties
 
